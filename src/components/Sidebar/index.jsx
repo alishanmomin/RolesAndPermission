6d4f8be3e77f080aa141cf43ea25a7742d6adf46
alishanmomin <mominalishan@gmail.com>
2023-06-06 17:53:12 +0500

    new

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 // import Navbar from '../Navbar'
 
-const Sidebar = ({index}) =>
+const Sidebar = ({ index }) =>
 {
+    const [open, setOpen] = useState(true);
     const navigate = useNavigate()
 
     const handleRoute = (route) =>
@@ -20,19 +21,26 @@ const Sidebar = ({index}) =>
     };
     return (
         <>
-            {<div className="sideBar sideBar_sidebarActive">
-                    <div className="sideBar_inner">
-                        <div onClick={() => handleRoute('articles')}  className={index === 1 ? "sideBar_title sideBar_active": "sideBar_title"}>
-                            <i className="fas fa-home"></i>Articles
-                        </div>
-                        <div onClick={() => handleRoute('roles')}  className={index === 2 ? "sideBar_title sideBar_active": "sideBar_title"}>
-                            <i className="fas fa-user"></i>Roles
-                        </div>
-                        <div onClick={() => handleRoute('logout')} className="sideBar_title">
-                            <i className="fas fa-power-off"></i>Logout
-                        </div>
+            <div className='navBar'>
+                <div className="navBar_left">
+                    <i className='fas fa-bars' onClick={() => setOpen(prev => !prev)}></i>
+                </div>
+                <div className="navBar_right">
+                </div>
+            </div>
+            {<div className={!open ? "sideBar" : "sideBar sideBar_sidebarActive"}>
+                <div className={open ? "sideBar_inner" : "sideBar_innersmall"}>
+                    <div onClick={() => handleRoute('articles')} className={index === 1 ? "sideBar_title sideBar_active" : "sideBar_title"}>
+                        <i className="fas fa-home"></i>Articles
+                    </div>
+                    <div onClick={() => handleRoute('roles')} className={index === 2 ? "sideBar_title sideBar_active" : "sideBar_title"}>
+                        <i className="fas fa-user"></i>Roles
+                    </div>
+                    <div onClick={() => handleRoute('logout')} className="sideBar_title">
+                        <i className="fas fa-power-off"></i>Logout
                     </div>
                 </div>
+            </div>
             }
         </>
     );
